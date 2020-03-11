f5fa8402e6d7025f5d127bdf6726de770d9a4390
大川峻 <ookawashun@ookawashunnoMacBook-Pro.local>
2020-03-11 18:35:43 +0900

    生徒用の問題の値を配列に格納する処理を追加
    先生用の表示として答えを出力する部分は未実装

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -9,10 +9,66 @@
     <header>
     </header>
     <main>
-    <h1>筆算の問題を出力します</h1>
+    <h1>足し算の筆算の問題を出力します</h1>
+
+    <!-- 計算問題の出力(答え無し) -->
+    <h3>計算問題</h3>
+    <!-- 配列の初期化 -->
+    <?php $num_1_array = []; ?>
+    <?php $num_2_array = []; ?>
+
+    <!-- for文の条件指定 10問出題したいので<=10に設定 -->
     @for($i = 1; $i <= 10; $i++)
+    
+        <!-- 問題数を出力 -->
         <h4>{{$i}}</h4>
+    
+        <!-- 1~99までの乱数を二つ生成 -->
+        <?php $num_1 = random_int(1, 99); ?>
+        <?php $num_2 = random_int(1, 99); ?>
+    
+        <!-- 生成された乱数を足した値を変数$num_totalに格納 -->
+        <?php $num_total = $num_1 + $num_2; ?>
+    
+        <!-- num_1の乱数をそれぞれ配列に格納 -->
+        @if(isset($num_1_array[0]))
+        
+        <!-- 配列の0番目に値が格納されている場合、末尾に追加 -->
+        <?php array_push($num_1_array, $num_1); ?>
+        @else
+        <!-- 配列の0番目に値が格納されていない場合、最初の値として追加 -->
+        <?php $num_1_array = [$num_1]; ?>
+        @endif
+    
+        <!-- num_2の乱数をそれぞれ配列に格納 -->
+        @if(isset($num_2_array[0]))
+        <?php array_push($num_2_array, $num_2); ?>
+        @else
+        <?php $num_2_array = [$num_2]; ?>
+        @endif
+
+        <!-- num_totalの値をそれぞれ配列に格納 -->
+        @if(isset($num_total_array[0]))
+        <?php array_push($num_total_array, $num_total); ?>
+        @else
+        <?php $num_total_array = [$num_total]; ?>
+        @endif
+
+        <!-- 計算式の出力 -->
+        <p>
+          {{$num_1}}<br>
+          {{$num_2}}<br>
+        +__<br>
+        </p>
     @endfor
+
+    <!-- 確認用そのうち消す -->
+    <?php var_dump($num_1_array); ?><br>
+    <?php var_dump($num_2_array); ?><br>
+    <?php var_dump($num_total_array); ?>
+
+    <!-- 計算問題の出力(答え有り) -->
+    <h3>計算問題の回答</h3>
     </main>
     <footer>
     </footer>
