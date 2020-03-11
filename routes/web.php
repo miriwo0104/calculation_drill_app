31bf4a099ae20b2753f17aabd4336353907c0500
大川峻 <ookawashun@ookawashunnoMacBook-Pro.local>
2020-03-11 12:43:59 +0900

    top画面のURLを修正した

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+#Route::get('/', function () {
+#    return view('welcome');
+#});
 
-Route::get('/top', 'TopController@index');
+Route::get('/', 'TopController@index');
 Route::get('/post', 'PostController@index');
