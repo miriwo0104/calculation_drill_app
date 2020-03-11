b1023764c108f27241a363bceb9ca61e69b90101
大川峻 <ookawashun@ookawashunnoMacBook-Pro.local>
2020-03-11 11:41:47 +0900

    Topページの文言実装を行った
    文言の実装が完了しただけ、デザインは何もいじってない
    全部左に寄っている
    「使ってみる」のボタンの実装まで完了した

---
+++ resources/views/top/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>topページ</title>
+</head>
+<body>
+    <header>
+    </header>
+    <main>
+    <h1>計算問題出力サービスへようこそ</h1>
+    <p>このサービスでは筆算の問題をランダムで出力することができます。</p>
+    <a href="/posts">
+        <button type="button">使ってみる</button>
+    </a>
+    </main>
+    <footer>
+    </footer>
+</body>
+</html>
