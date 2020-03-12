79cefede530cf9ccc2c47286380c913da210f96b
大川峻 <ookawashun@ookawashunnoMacBook-Pro.local>
2020-03-12 18:48:21 +0900

    footerをテンプレートファイルに記載した

--- resources/views/top/index.blade.php
+++ resources/views/top/index.blade.php
@@ -5,17 +5,9 @@
 @endsection
 
 @section('content')
-
-<h1>計算問題出力サービスへようこそ</h1>
-<p>このサービスでは筆算の問題をランダムで出力することができます。</p>
-<a href="/post">
-    <button type="button">使ってみる</button>
-</a>
-
+    <h1>計算問題出力サービスへようこそ</h1>
+    <p>このサービスでは筆算の問題をランダムで出力することができます。</p>
+    <a href="/post">
+        <button type="button">使ってみる</button>
+    </a>
 @endsection
-
-@section('footer')
-    <p>footerです</p>
-@endsection
-
-
