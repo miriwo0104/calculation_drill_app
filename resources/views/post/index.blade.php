e645cd35bb3931aeef55020c76b3fb782d208c6e
大川峻 <ookawashun@ookawashunnoMacBook-Pro.local>
2020-03-12 18:04:27 +0900

    レイアウト周りの修正を行った
    top画面もテンプレートレイアウトに対応させた
    headerのナビバーをテンプレートファイルに記載した

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -4,34 +4,6 @@
     計算問題出力
 @endsection
 
-@section('header')
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="/">計算問題集</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-      </ul>
-    </div>
-  </nav>
-@endsection
-
 @section('content')
 
     <!-- 計算問題の出力(答え無し) -->
