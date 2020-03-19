f8e38127ec6df891610e88584ff2e5979e93a765
miriwo0517 <shunokawa@gmail.com>
2020-03-19 12:46:02 +0900

    引き算の出力ページのビューの処理をコントローラに記載した

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -46,7 +46,42 @@ class PostController extends Controller
     }
 
     public function subtraction() {
-        return view('post.subtraction');
+        #ページタイトルの変数格納
+        $title = '引き算の計算問題出力';
+
+        # 配列の初期化
+        $data = [];
+
+        for ($i = 1; $i <= 10; $i++){
+            #足す数と足される数を乱数で取得
+            $num_1 = random_int(1, 99);
+            $num_2 = random_int(1, 99);
+
+            #引かれる数が大きくなるようにする処理
+            while ($num_1 < $num_2) {
+                $num_1 = random_int(1, 99);
+            }
+
+            #足し算の答えを変数に格納
+            $num_result = $num_1 - $num_2;
+
+            if (isset($data[0])){
+                array_push($data, [
+                    'num_1' => $num_1,
+                    'num_2' => $num_2,
+                    'num_result' => $num_result,
+                ]);
+            }else{
+                $data = [
+                    [
+                        'num_1' => $num_1,
+                        'num_2' => $num_2,
+                        'num_result' => $num_result,
+                    ]
+                ];
+            }
+        } 
+        return view('post.subtraction', compact('data'));
     }
 
     public function subtraction_answer() {
