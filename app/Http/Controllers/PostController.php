34a3bb7495dc1868891b2c38075a96c39e23d2b0
miriwo0517 <shunokawa@gmail.com>
2020-03-19 12:17:46 +0900

    掛け算の計算出力のビューファイルの記載をコントローラに切り替えて修正した

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -54,7 +54,37 @@ class PostController extends Controller
     }
 
     public function multiplication() {
-        return view('post.multiplication');
+        #ページタイトルの変数格納
+        $title = '足し算の計算問題出力';
+        # 配列の初期化
+        $data = [];
+
+        for ($i = 1; $i <= 10; $i++){
+            #足す数と足される数を乱数で取得
+            $num_1 = random_int(1, 99);
+            $num_2 = random_int(1, 99);
+
+            #足し算の答えを変数に格納
+            $num_result = $num_1 * $num_2;
+
+            if (isset($data[0])){
+                array_push($data, [
+                    'num_1' => $num_1,
+                    'num_2' => $num_2,
+                    'total' => $num_result,
+                ]);
+            }else{
+                $data = [
+                    [
+                        'num_1' => $num_1,
+                        'num_2' => $num_2,
+                        'total' => $num_result,
+                    ]
+                ];
+            }
+        }
+
+        return view('post.multiplication', compact('data'));
     }
 
     public function multiplication_answer() {
