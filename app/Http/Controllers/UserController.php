8f3f18d38e397b71304d0b5d2c255907586cb056
大川峻 <ookawashun@ookawashunnoMacBook-Pro.local>
2020-03-30 19:00:18 +0900

    ユーザホーム画面でユーザIDとユーザ名を出力する処理を追加
    機能未完

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,9 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+//下記がないとAuthの情報拾ってこられない
+use Auth;
+
 class UserController extends Controller
 {
     public function home() {
-        return view('user.home');
+        $user_id = Auth::id();
+        $user = \App\User::find($user_id);
+        $user_name = $user->name;
+        return view('user.home', compact('user_id'));
     }
 }
--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -1,5 +1,9 @@
-@extends('layout.application')
+@extends('layouts.application')
 
 @section('content')
     <h1>これはユーザホームです</h1>
+    @php
+        echo $user_id;
+        echo $user_name;
+    @endphp
 @endsection
