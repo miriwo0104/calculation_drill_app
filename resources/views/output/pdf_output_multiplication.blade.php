d8740384ad2743e6d191b98406af138380b2abcf
大川峻 <ookawashun@ookawashunnoMacBook-Pro.local>
2020-03-30 12:35:52 +0900

    Postコントローラの記載をOutputコントローラに修正した
    名前空間や諸々のファイルのPostの記載をOutputに修正した

---
+++ resources/views/output/pdf_output_multiplication.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.pdf')
+
+@section('title')
+    掛け算
+@endsection
+
+@section('content')
+
+    <!-- 配列に入った連想配列の中身を出力する -->
+    @foreach ($data as $datum)
+        <p>
+            {{$datum['num_1']}}<br>
+            {{$datum['num_2']}}<br>
+            ×__<br>
+        </p>
+    @endforeach
+    <p>
+        答え
+    </p>
+    @foreach ($data as $datum)
+        <p>
+            {{$datum['num_1']}}<br>
+            {{$datum['num_2']}}<br>
+            ×__<br>
+            {{$datum['num_result']}}<br>
+        </p>
+    @endforeach
+@endsection
---
+++ resources/views/output/test.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<title>PDF</title>
+<style>
+body {
+font-family: ipag;
+}
+</style>
+</head>
+<body>
+    <h1>こんにちは</h1>
+</div>
+</body>
+</html>
